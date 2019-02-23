1d91f6b75907a8f60aef1fa298c37dc681e976d0
Erik Hebisch <erik.hebisch@gmail.com>
2019-02-24 00:02:37 +0100

    Cleaned up title fetching script

--- fetch-titles.rb
+++ fetch-titles.rb
@@ -2,19 +2,21 @@ require 'yaml/store'
 require 'open-uri'
 require 'pp'
 
+DEFAULT_TITLE = "No Title (possible 404)"
+
 def get_title(url)
   scan = open(url).read.scan(/<title>(.*?)<\/title>/).first.first
   p scan
   scan
 rescue OpenURI::HTTPError, NoMethodError
-  "No Title (possible 404)" 
+  DEFAULT_TITLE
 end
 
 store = YAML::Store.new('db.yml')
 urls = store.transaction { store[:urls] }
 newrls = []
 urls.each do |u| 
-  if u[:title] == "No Title (possible 404)"
+  if u[:title] == DEFAULT_TITLE
     t = {}; 
     t[:title] = get_title(u[:url]); 
     t[:url] = u[:url];
